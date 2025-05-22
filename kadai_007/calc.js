0025441339f1383dfb94adbf05510bb6eca779f8
shuna421 <shuna2001@icloud.com>
2025-05-22 15:06:30 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,19 @@
+let num = Math.floor(Math.random()*15);
+
+if(num===3){
+  console.log('3の倍数です');
+}else{
+  console.log('num');
+}
+
+if(num===5){
+  console.log('5の倍数です');
+}else{
+  console.log('num');
+}
+
+if(num===3,num===5){
+  console.log('3と5の倍数です');
+}else{
+  console.log('num')
+}
